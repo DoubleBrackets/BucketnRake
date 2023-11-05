f665d8f528425442a66f0f7271ee2b05c30bce8b
MSI\arthu <arthurgan29@gmail.com>
2023-11-05 01:59:44 -0700

    added doors and levers

---
+++ LeafLegends/Assets/Scripts/LevelMapping/GenericLever.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GenericLever : MonoBehaviour
+{
+    public UnityEvent OnLeverPulled;
+
+    public SimpleAnimator Animator;
+
+    private bool pulled;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !pulled)
+        {
+            pulled = true;
+            OnLeverPulled?.Invoke();
+            Animator.Play();
+        }
+    }
+}
